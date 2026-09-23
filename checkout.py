b4dd90646440d8ce18ad6c8994294f77bdc21d7f
Eliadsp <eliad@umich.edu>
2026-09-22 20:00:15 -0400

    Create checkout.py

---
+++ checkout.py
@@ -0,0 +1,8 @@
+def checkout(prices):
+    """Return the total cost of the supplied prices."""
+    total = sum(prices)
+    return total
+
+
+if __name__ == "__main__":
+    print(checkout([4, 6]))
